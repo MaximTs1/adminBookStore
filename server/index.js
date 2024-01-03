aaeb14b1f835a6a302790bdc3b702051e4bb1240
maxim <maxim526@gmail.com>
2024-01-03 17:30:25 +0200

    index.js server change

--- server/index.js
+++ server/index.js
@@ -14,18 +14,37 @@ mongoose
 
 const app = express();
 const port = 3001;
+
 app.use(express.json());
 
+// Dynamic CORS configuration
 app.use(
   cors({
-    origin: true,
+    origin: function (origin, callback) {
+      // Allow requests with no origin
+      if (!origin) return callback(null, true);
+
+      // Check if the origin is in the list of allowed origins
+      if (
+        origin.startsWith("http://localhost:") ||
+        origin.startsWith("https://localhost:")
+      ) {
+        return callback(null, true);
+      }
+
+      // Disallow other origins
+      return callback(
+        new Error("CORS policy does not allow this origin"),
+        false
+      );
+    },
     credentials: true,
     methods: "GET,PUT,POST,DELETE",
     allowedHeaders: "Content-Type, Accept, Authorization",
   })
 );
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use("/api", bookRoutes);
 app.use("/user", userRoutes);
